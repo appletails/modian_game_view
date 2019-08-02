474ae9fb2af6c724fc5dbcb53cea130019fd2667
appletails <xuyi19920401@qq.com>
2019-08-02 18:50:44 +0800

    拆分UserIdol

---
+++ src/components/UserIdol.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="line">
+      <span :class="['one',item.life?'':'error']">{{item.nickname}}</span>
+      <span class="two">{{item.level}}</span>
+      <span class="three">{{item.star | stars}}</span>
+      <span class="fore">数量：{{item.num}}</span>
+    </div>
+    <div class="line">
+      <span class="one">攻：{{item.attack}}</span>
+      <span class="two">防：{{item.defense}}</span>
+      <span class="three">
+        血：
+        <span :class="[item.life?'':'error']">{{item.life}}</span>
+        /{{item.alllife}}
+      </span>
+      <span :class="['fore',item.skill == '未知'?'':'warning']">{{item.skill}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import { stars } from "@/filters";
+export default {
+  name: "UserIdol",
+  props: ["item"],
+  filters: {
+    stars
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.line {
+  .flex;
+  span {
+    padding: 5px 0;
+    line-height: 1.5rem;
+    text-align: center;
+    &:not(:last-of-type) {
+      border-right: 1px solid @border;
+    }
+  }
+  .one {
+    width: 22%;
+  }
+  .two {
+    width: 24%;
+  }
+  .three {
+    width: 31%;
+  }
+  .fore {
+    width: 25%;
+  }
+  &:first-of-type {
+    border-bottom: 1px solid @border;
+  }
+}
+</style>
--- src/page/User/index.vue
+++ src/page/User/index.vue
@@ -2,147 +2,115 @@
   <div>
     <div class="top">
       <div class="nickname">
-        <span>{{user.nickname[0]}}</span>
-        <span v-if="user.nickname[1]">[{{user.nickname[1]}}]</span>
+        <span>{{nickname}}</span>
       </div>
       <button @click="$router.push('/')" class="tail_btn">返回</button>
     </div>
     <div class="btn_line">
-        <button :class="[active == 0?'tail_btn_info':'','tail_btn']" @click="paixu(0)">稀有度</button>
-        <button :class="[active == 1?'tail_btn_info':'','tail_btn']" @click="paixu(1)">星级</button>
-        <button :class="[active == 2?'tail_btn_info':'','tail_btn']" @click="paixu(2)">攻击</button>
-        <button :class="[active == 3?'tail_btn_info':'','tail_btn']" @click="paixu(3)">防御</button>
-        <button :class="[active == 4?'tail_btn_info':'','tail_btn']" @click="paixu(4)">生命</button>
-        <button :class="[active == 5?'tail_btn_info':'','tail_btn']" @click="paixu(5)">数量</button>
-        <button :class="[active == 6?'tail_btn_info':'','tail_btn']" @click="paixu(6)">技能</button>
+      <button :class="[active == 0?'tail_btn_info':'','tail_btn']" @click="paixu(0)">稀有度</button>
+      <button :class="[active == 1?'tail_btn_info':'','tail_btn']" @click="paixu(1)">星级</button>
+      <button :class="[active == 2?'tail_btn_info':'','tail_btn']" @click="paixu(2)">攻击</button>
+      <button :class="[active == 3?'tail_btn_info':'','tail_btn']" @click="paixu(3)">防御</button>
+      <button :class="[active == 4?'tail_btn_info':'','tail_btn']" @click="paixu(4)">生命</button>
+      <button :class="[active == 5?'tail_btn_info':'','tail_btn']" @click="paixu(5)">数量</button>
+      <button :class="[active == 6?'tail_btn_info':'','tail_btn']" @click="paixu(6)">技能</button>
     </div>
     <ul>
       <li v-for="(item, index) in user.idol" :key="index">
-        <div>
-          <span :class="['one',item.life?'':'error']">{{item.nickname}}</span>
-          <span class="two">{{item.level}}</span>
-          <span class="three">{{item.star | stars}}</span>
-          <span class="fore">数量：{{item.num}}</span>
-        </div>
-        <div>
-          <span class="one">攻：{{item.attack}}</span>
-          <span class="two">防：{{item.defense}}</span>
-          <span class="three">血：<span :class="[item.life?'':'error']">{{item.life}}</span>/{{item.alllife}}</span>
-          <span :class="['fore',item.skill == '未知'?'':'warning']">{{item.skill}}</span>
-        </div>
+        <UserIdol :item="item" />
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-import { stars } from '@/filters'
+import UserIdol from "@/components/UserIdol";
 export default {
   components: {
+    UserIdol
   },
-  filters: {stars},
-  data () {
+  data() {
     return {
-      user: {
-        nickname: []
-      },
+      user: {},
+      nickname: [],
       active: null
-    }
+    };
   },
   methods: {
-    async paixu (i) {
-      const lvUp = {UR: 4, SSR: 3, SR: 2, R: 1, N: 0}
+    async paixu(i) {
+      const lvUp = { UR: 4, SSR: 3, SR: 2, R: 1, N: 0 };
       switch (i) {
         case 0:
-          this.user.idol = this.user.idol.sort((a, b) => lvUp[b.level] - lvUp[a.level])
-          break
+          this.user.idol = this.user.idol.sort(
+            (a, b) => lvUp[b.level] - lvUp[a.level]
+          );
+          break;
         case 1:
-          this.user.idol = this.user.idol.sort((a, b) => b.star - a.star)
-          break
+          this.user.idol = this.user.idol.sort((a, b) => b.star - a.star);
+          break;
         case 2:
-          this.user.idol = this.user.idol.sort((a, b) => b.attack - a.attack)
-          break
+          this.user.idol = this.user.idol.sort((a, b) => b.attack - a.attack);
+          break;
         case 3:
-          this.user.idol = this.user.idol.sort((a, b) => b.defense - a.defense)
-          break
+          this.user.idol = this.user.idol.sort((a, b) => b.defense - a.defense);
+          break;
         case 4:
-          this.user.idol = this.user.idol.sort((a, b) => b.life - a.life)
-          break
+          this.user.idol = this.user.idol.sort((a, b) => b.life - a.life);
+          break;
         case 5:
-          this.user.idol = this.user.idol.sort((a, b) => b.num - a.num)
-          break
+          this.user.idol = this.user.idol.sort((a, b) => b.num - a.num);
+          break;
         case 6:
-          const skills = await this.$account.getSkill()
+          const skills = await this.$account.getSkill();
           this.user.idol = this.user.idol.sort((a, b) => {
-            return skills[b.skill] - skills[a.skill]
-          })
-          break
+            return skills[b.skill] - skills[a.skill];
+          });
+          break;
       }
-      this.active = i
+      this.active = i;
     }
   },
-  async created () {
-    const response = await this.$account.getOneUser(this.$route.query.nickname)
-    this.user = response.data
+  async created() {
+    this.nickname = this.$route.query.nickname;
+    const response = await this.$account.getOneUser(this.nickname);
+    if (response.code !== 200) {
+      alert(`查找失败：${response.msg}`);
+      this.$router.go(-1);
+    }
+    this.user = response.data;
   }
-}
+};
 </script>
 
 <style lang="less" scoped>
-.top{
+.top {
   .flex;
   padding: 10px;
   background-color: #fff;
   border-bottom: 1px solid @border;
-  .nickname{
+  .nickname {
     font-size: 16px;
     font-weight: bold;
   }
 }
-.btn_line{
+.btn_line {
   .flex;
   flex-wrap: wrap;
   margin: 10px;
-  button{
+  button {
     margin-bottom: 5px;
   }
 }
-ul{
+ul {
   margin: 10px;
-  li{
+  li {
     // .flex;
     font-size: 14px;
     justify-content: flex-start;
     padding: 5px;
     margin-top: 10px;
-    border:1px solid @border;
+    border: 1px solid @border;
     border-radius: 6px;
-    div{
-      .flex;
-      span{
-        padding:5px 0;
-        line-height: 1.5rem;
-        text-align: center;
-        &:not(:last-of-type){
-          border-right: 1px solid @border;
-        }
-      }
-      .one{
-        width:22%;
-      }
-      .two{
-        width:24%;
-      }
-      .three{
-        width:31%;
-      }
-      .fore{
-        width:25%;
-      }
-      &:first-of-type{
-        border-bottom: 1px solid @border;
-      }
-    }
   }
 }
 </style>
